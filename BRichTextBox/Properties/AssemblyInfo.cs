2801976fcab2b19d7408ae4481ab2f877324f214
Boaznk <boaznk@gmail.com>
2025-11-20 04:49:36 +0200

    Moved files

--- BRichTextBox/Properties/AssemblyInfo.cs
+++ BRichTextBox/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BRichTextBox")]
+[assembly: AssemblyTitle("bRichTextBox")]
 [assembly: AssemblyDescription("Extended RichTextBox with auto scroll, append line, append exc and auto date")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Boaz")]
-[assembly: AssemblyProduct("BRichTextBox")]
+[assembly: AssemblyProduct("bRichTextBox")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("None")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.5.1")]
+[assembly: AssemblyFileVersion("1.0.5.1")]
--- BRichTextBox/README.txt
+++ BRichTextBox/README.txt
@@ -1,1 +1,1 @@
-﻿Extended RichTextBox with auto scroll, append line, append exc and auto date
+﻿Extended RichTextBox with auto scroll, append line, append exc and auto date.
